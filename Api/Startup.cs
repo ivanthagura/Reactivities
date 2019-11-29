dc94cb458cc0ac5dba7640552216b757c55f2f0a
Ivantha Guruge <ivantha88@hotmail.com>
2019-11-29 21:37:16 +1100

    create client app

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -31,6 +31,13 @@ namespace Api
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -47,6 +54,8 @@ namespace Api
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
